d5ad26f593df22cdeb630dc2f9474b1a911846d6
jonbrand <jonbranaraujo18@gmail.com>
2023-03-02 11:16:27 -0300

    feat: add exercise

--- exercises/click-browser-app/src/App.js
+++ exercises/click-browser-app/src/App.js
@@ -1,24 +1,43 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+
 import './App.css';
 
 function App() {
+  const [clickedPoints, setClickedPoints] = useState([]);
+
+  function screenCoordinates(e) {
+    const { clientX, clientY } = e;
+
+    setClickedPoints([...clickedPoints, { clientX, clientY }]);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App" onClick={screenCoordinates}>
+        <button className='undo' onClick={''}>Undo</button>
+        <button className='redo' onClick={''}>Redo</button>
+        {clickedPoints.map((clickedPoint) => {
+          return (
+          <div 
+            style={{
+              left: clickedPoint.clientX -50, 
+              top: clickedPoint.clientY -50,
+              position: "absolute",
+              borderRadius: "50%",
+              background: "blue",
+              width: "100px",
+              height: "100px",
+              animationName: 'fadeInOut',
+              animationDuration: "10s",
+              animationDelay: "2s"
+
+            }}
+          >
+          </div>
+          )
+        })}
+      </div>
+    </>
   );
 }
 
